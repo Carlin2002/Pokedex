78349f1acb0613351a30e53ba02d309d5d1c8cc6
carlin2002 <carlo.radecki@hotmail.com>
2023-10-31 15:38:28 -0300

    Good habits

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const fetchPokemon = async (pokemon) => {
 const renderPokemon = async (pokemon) => {
 
   pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-   'ghost', 'steel', 'dragon', 'dark', 'fairy');
+  'ghost', 'steel', 'dragon', 'dark', 'fairy');
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = '';
 
@@ -30,116 +30,116 @@ const renderPokemon = async (pokemon) => {
   if (data) {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['front_default']
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['front_default'];
   
     input.value = '';
     searchPokemon = data.id;
     
     if (data['types']['0']['type']['name'] === 'normal') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('normal');
     }
 
     if (data['types']['0']['type']['name'] === 'water') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('water');
     }
 
     if (data['types']['0']['type']['name'] === 'fire') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('fire');
     }
 
     if (data['types']['0']['type']['name'] === 'grass') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('grass');
     }
 
     if (data['types']['0']['type']['name'] === 'flying') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('flying');
     }
 
     if (data['types']['0']['type']['name'] === 'fighting') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('fighting');
     }
 
     if (data['types']['0']['type']['name'] === 'poison') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('poison');
     }
 
     if (data['types']['0']['type']['name'] === 'electric') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('electric');
     }
 
     if (data['types']['0']['type']['name'] === 'ground') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('ground');
     }
 
     if (data['types']['0']['type']['name'] === 'rock') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('rock');
     }
 
     if (data['types']['0']['type']['name'] === 'psychic') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('psychic');
     }
 
     if (data['types']['0']['type']['name'] === 'ice') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('ice');
     }
 
     if (data['types']['0']['type']['name'] === 'bug') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('bug');
     }
 
     if (data['types']['0']['type']['name'] === 'ghost') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('ghost');
     }
 
     if (data['types']['0']['type']['name'] === 'steel') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('steel');
     }
 
     if (data['types']['0']['type']['name'] === 'dragon') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('dragon');
     }
 
     if (data['types']['0']['type']['name'] === 'dark') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('dark');
     }
 
     if (data['types']['0']['type']['name'] === 'fairy') {
       pokemonName.classList.remove('normal', 'water', 'fire', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug',
-       'ghost', 'steel', 'dragon', 'dark', 'fairy');
+      'ghost', 'steel', 'dragon', 'dark', 'fairy');
       pokemonName.classList.add('fairy');
     }
     
